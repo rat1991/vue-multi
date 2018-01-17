6ea8dc562f22cc11ee0800fbccd3ea24f51fa75e
rat1991 <k93945354@gmail.com>
2018-01-17 10:53:29 +0800

    修改配置文件

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@
 // Template version: 1.2.7
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-/*========项目名称=========*/ 
+/*========项目名称=========*/
 const projectName = 'demo'
-/*=========END===========*/ 
+/*=========END===========*/
 
 const path = require('path')
 
@@ -26,7 +26,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '10.100.83.83', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 3000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
